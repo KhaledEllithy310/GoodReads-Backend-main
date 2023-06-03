86db0888e0f01913c913ca0199c4089c9f1a62de
ShymaaKelany <kelanymmd123@gmail.com>
2023-06-03 09:28:20 +0300

    add edit of db

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const ratesRoute = require("./routes/rates")
 //CATECORY AND AUTHOR ROUTES
 const categoryRouter = require("./routes/category");
 const authorRouter = require("./routes/author");
-const port = 8080;
+const port = 808
+0;
 app.use(cors());
 app.use(express.json());
 /**********************************************************************/
@@ -24,7 +25,7 @@ app.use("/register", registerRoute);
 app.use("/login", loginRoute);
 app.use("/userbooks", userBooksRoutes);
 app.use("/reviews", reviewsRoute);
-app.use(express.static("assets")); //show images in chrome
+app.use(express.static('assets')); //show images in chrome
 app.use("/category", categoryRouter);
 app.use("/author", authorRouter);
 app.use('/books',bookRoute)
@@ -38,7 +39,7 @@ app.post("/welcome", auth, (req, res) => {
 mongoose.set("strictQuery", false);
 //CONNECT WITH DATA BASE
 mongoose.connect(
-  "mongodb://127.0.0.1:27017/readApp",
+  "mongodb+srv://khaled-Ellithy:meanstack@cluster0.xopyoua.mongodb.net/GoodReads?retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err) => {
     if (!err) {
